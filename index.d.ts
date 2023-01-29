b7b6ea38000295fbdbd69f275301d17da36858ff
@e53e04ac <e53e04ac@gmail.com>
2023-01-29 14:39:24 +0900

    0

--- index.d.ts
+++ index.d.ts
@@ -47,3 +47,129 @@ export declare type Ipp5BackendStaticEnvMap = {
     readonly CUSTOM_EXPRESS_SUBDOMAIN_OFFSET: number;
     readonly CUSTOM_EXPRESS_TRUST_PROXY: string;
 };
+
+export declare type Ipp5SampleData<Value> = {
+    type: string;
+    timeRangeType: string;
+    timeRangeStartDate: number;
+    timeRangeEndDate: number;
+    year?: number;
+    month?: number;
+    day?: number;
+    hour?: number;
+    minute?: number;
+    second?: number;
+    value: Value;
+    metricUnit?: string;
+    error?: Error;
+    rawValue?: unknown;
+};
+
+export declare type Ipp5MenuData = {
+    readonly error?: Error;
+    readonly items: ({
+        readonly type: string;
+    } & ({
+        readonly type: 'divider';
+    } | {
+        readonly type: 'header';
+        readonly text: string;
+    } | {
+        readonly type: 'item';
+        readonly icon: string;
+        readonly text: string;
+        readonly href: string;
+    } | {
+        readonly type: 'group';
+        readonly text: string;
+        readonly subitems: ({
+            readonly type: string;
+        } & ({
+            readonly type: 'header';
+            readonly text: string;
+        } | {
+            readonly type: 'item';
+            readonly text: string;
+            readonly href: string;
+        }))[];
+    }))[];
+};
+
+export declare type Ipp5YearlySummaryData = {
+    readonly error?: Error;
+    readonly year: number;
+    readonly months: number[];
+    readonly durationOfSunshine: {
+        readonly monthlySamples: Ipp5SampleData<number>[];
+    };
+    readonly monthlyPowerGenerationInThisYear: {
+        readonly monthlySamples: Ipp5SampleData<number>[];
+    };
+    readonly monthlyPowerGenerationGainRatio: {
+        readonly monthlySamples: Ipp5SampleData<number>[];
+    };
+    readonly plants: {
+        readonly index: number;
+        readonly name: string;
+        readonly monthlySamples: Ipp5SampleData<number>[];
+    }[];
+};
+
+export declare type Ipp5MonthlySummaryData = {
+    readonly error?: Error;
+    readonly year: number;
+    readonly month: number;
+    readonly days: number[];
+    readonly weatherIcon: {
+        readonly dailySamples: Ipp5SampleData<string>[];
+    };
+    readonly weatherDescription: {
+        readonly dailySamples: Ipp5SampleData<string>[];
+    };
+    readonly averageTemperture: {
+        readonly dailySamples: Ipp5SampleData<number>[];
+    };
+    readonly durationOfSunshine: {
+        readonly dailySamples: Ipp5SampleData<number>[];
+    };
+    readonly plants: {
+        readonly index: number;
+        readonly name: string;
+        readonly dailySamples: Ipp5SampleData<number>[];
+    }[];
+};
+
+export declare type Ipp5DailySummaryData = {
+    readonly error?: Error;
+    readonly year: number;
+    readonly month: number;
+    readonly day: number;
+    readonly dailyPowerGeneration: {
+        readonly dailySample: Ipp5SampleData<number>;
+    };
+    readonly monthlyPowerGeneration: {
+        readonly monthlySample: Ipp5SampleData<number>;
+    };
+    readonly averageTemperture: {
+        readonly dailySample: Ipp5SampleData<number>;
+    };
+    readonly durationOfSunshine: {
+        readonly dailySample: Ipp5SampleData<number>;
+    };
+    readonly weatherIcon: {
+        readonly dailySample: Ipp5SampleData<string>;
+    };
+    readonly greenCount: number;
+    readonly yellowCount: number;
+    readonly redCount: number;
+    readonly plants: {
+        readonly index: number;
+        readonly name: string;
+        readonly red: boolean;
+        readonly yellow: boolean;
+        readonly green: boolean;
+        readonly hourlySamples: Ipp5SampleData<number>[];
+        readonly dailySamples: Ipp5SampleData<number>[];
+        readonly dailySample: Ipp5SampleData<number>;
+    }[];
+};
